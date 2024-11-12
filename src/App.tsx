3e81ec44fed41b45490d5412ee2966647d8c4fa6
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-11-12 20:55:59 -0300

    feat: Trabalhando no projeto `Pomodoro` - parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import { PomodoroTimer } from './components/pomodoro-timer'
 function App(): JSX.Element {
     return (
         <main>
-            <h1>Hollo world.!!!</h1>
-            <PomodoroTimer defaultPomodoroTime={1500}/>
+            <section className='main-container'>
+                <h1>Pomodoro</h1>
+                <PomodoroTimer PomodoroTime={1500}
+                shortRestTime={300}
+                longRestTime={900}
+                cycles={4}/>
+            </section>
         </main>
     );
 }
